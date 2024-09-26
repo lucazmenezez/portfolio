54c6e2677160d7c2f37dbf30eaffc2d4c952a3cf
lucas menezes <menezeslucas24082002@gmail.com>
2024-09-26 08:07:08 -0300

    Configurando o ambiente de desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import EstiloGlobal from './styles'
+import Teste from './teste'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Teste />
+    </>
+  )
+}
+
+export default App
