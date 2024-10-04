1baf6580cf22db34b7b23cc7712a548ef74c0c9f
lucas menezes <menezeslucas24082002@gmail.com>
2024-10-04 10:05:23 -0300

    Construindo a estrutura e a seção 'sobre'

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,13 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img
+      src="https://github.com/lucazmenezez.png"
+      alt="foto de perfil do github"
+    />
+    <Titulo fontSize={20}>Lucas Menezes</Titulo>
+  </aside>
+)
+
+export default Sidebar
